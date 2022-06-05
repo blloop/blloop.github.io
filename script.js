9299e00bbab4d71c2a2d3517a05ae66c7546b34f
blloop <blloop@uw.edu>
2022-06-04 22:42:46 -0700

    Complete website revamp

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Script file for interactive components
 
-const viewScroller = document.querySelectorAll('[data-slide]');
+// const viewScroller = document.querySelectorAll('[data-slide]');
 
-viewScroller.forEach(button => {
-	button.addEventListener('click', () => {
-		const loc = document.querySelector(button.dataset.slide);
-		loc.scrollIntoView({behavior: "smooth"});
-	});
-});
+// viewScroller.forEach(button => {
+// 	button.addEventListener('click', () => {
+// 		const loc = document.querySelector(button.dataset.slide);
+// 		loc.scrollIntoView({behavior: "smooth"});
+// 	});
+// });
