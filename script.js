e58d907d19d3761afb0523b31bb1e9fc59fbf6b4
blloop <42186044+blloop@users.noreply.github.com>
2022-07-10 21:07:07 -0700

    Add icons to screen

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 // Script file for interactive components
 
-// const viewScroller = document.querySelectorAll('[data-slide]');
+// Render desktop icons from icon.js file
+output = "";
+for (i in icons) {
+    output += `<div class='icon' id="${i}" onclick="select(${i})">`;
+    output += `<img src="img/${icons[i].img}">`;
+    output += `<p>${icons[i].title}</p>`;
+    output += "</div>";
+}
+document.getElementById("icons").innerHTML = output;
 
-// viewScroller.forEach(button => {
-// 	button.addEventListener('click', () => {
-// 		const loc = document.querySelector(button.dataset.slide);
-// 		loc.scrollIntoView({behavior: "smooth"});
-// 	});
-// });
+// Highlights selected icon with a border
+
+var selected = undefined;
+
+function select(num) {
+    if (selected !== num) {
+        if (selected !== undefined) {
+            document.getElementById(selected).classList.remove('select');
+        }        
+        if (num !== undefined) {
+            document.getElementById(num).classList.add('select');
+        }        
+        selected = num;
+    }
+};
+
+// Clears selected icons by clicking outside on the desktop
+// Currently not functioning
+
+function clearSelect() {
+    // if (selected !== undefined) {
+    //     document.getElementById(selected).classList.remove('select');
+    // }
+    // selected = undefined;
+    
+}
