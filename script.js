cbacb52be1153454982bb0a7c852c799f51a46ea
blloop <42186044+blloop@users.noreply.github.com>
2022-09-21 20:10:44 -0700

    Page restructure

--- script.js
+++ script.js
@@ -1,76 +1,39 @@
-// Script file for interactive components
-// document.querySelectorAll("body").imageSmoothingEnabled = false;
+const openPopup = document.querySelectorAll('[data-open]');
+const closePopup = document.querySelectorAll('[data-close]');
+const overlay = document.getElementById('overlay');
 
-// Render desktop icons from icon.js file
-output = "";
-for (i in icons) {
-    output += `<div class='icon' id="${i}" onclick="select(${i})">`;
-    output += `<img src="img/${icons[i].img}">`;
-    output += `<p>${icons[i].title}</p>`;
-    output += "</div>";
-}
-document.getElementById("icons").innerHTML = output;
+// Open a popup
+openPopup.forEach(button => {
+  button.addEventListener('click', () => {
+    const pop = document.querySelector(button.dataset.open);
+    open_popup(pop);
+  });
+});
 
-// Highlights selected icon with a border
+// Exit button to close
+closePopup.forEach(button => {
+  button.addEventListener('click', () => {
+    const pop = document.querySelector(button.dataset.close);
+    close_popup(pop);
+  });
+});
 
-var selected = undefined;
+// Outside click to close
+overlay.addEventListener('click', () => {
+  const pop = document.querySelectorAll('.active');
+  pop.forEach(i => {
+    close_popup(i);
+  });
+});
 
-function select(num) {
-    if (selected !== num) {
-        if (selected !== undefined) {
-            document.getElementById(selected).classList.remove('select');
-        }        
-        if (num !== undefined) {
-            document.getElementById(num).classList.add('select');
-        }        
-        selected = num;
-    }
+function open_popup(element) {
+  if (element == null) return
+  element.classList.add('active');
+  overlay.classList.add('active');
 };
 
-// Clears selected icons by clicking outside on the desktop
-// Currently not functioning
-
-function clearSelect() {
-    // if (selected !== undefined) {
-    //     document.getElementById(selected).classList.remove('select');
-    // }
-    // selected = undefined;
-    
-}
-
-// Toggles start panel with start button
-
-var startOpen = false;
-
-function toggleStart() {
-
-    if (startOpen) {
-        document.getElementById("start_panel").classList.remove('start_open');
-        document.getElementById("start").classList.remove('start_clicked');
-    }
-    else {
-        document.getElementById("start_panel").classList.add('start_open');
-        document.getElementById("start").classList.add('start_clicked');
-    }
-    startOpen = !startOpen;
-}
-
-// Sets system time to the browser's time
-var current = new Date();
-
-if (current.getHours() > 12) {
-    current.setHours(current.getHours() - 12);
-    ampm = "PM";
-} else {
-    ampm = "AM";
-}
-
-var time = `${current.getHours()}:`;
-if (current.getMinutes() < 10) {
-    time += "0";
-}
-time += current.getMinutes() + ampm;
-console.log("System Time: " + time);
-
-time = "<img src='img/speaker.png'><div>" + time + '</div';
-document.getElementById("time").innerHTML = time;
+function close_popup(element) {
+  if (element == null) return
+  element.classList.remove('active');
+  overlay.classList.remove('active');
+};
