13861428062088504e68a60c26280d10ef6cd2f1
blloop <42186044+blloop@users.noreply.github.com>
2022-12-20 00:50:35 -0800

    improve light mode ui

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ function toggle_color() {
 };
 
 function set_light() {
-    document.getElementById("toggle_button").innerText = "LIGHT MODE";
+    document.getElementById("lightbulb").src = "img/light_off.png";
     document.body.style.backgroundColor = 'rgb(215, 215, 215)';
-    document.getElementById("toggle_button").style.backgroundColor = 'rgb(215, 215, 215)';
+    // document.getElementById("toggle_button").style.backgroundColor = 'rgb(50, 50, 50)';
     document.querySelectorAll("*").forEach(doc => doc.style.color = 'rgb(50, 50, 50)');
     document.querySelectorAll("hr").forEach(doc => doc.style.backgroundColor = 'rgb(50, 50, 50)');
     current_mode = "light";
 }
 
 function set_dark() {
-    document.getElementById("toggle_button").innerText = "DARK MODE";
+    document.getElementById("lightbulb").src = "img/light_on.png";
     document.body.style.backgroundColor = 'rgb(50, 50, 50)';
-    document.getElementById("toggle_button").style.backgroundColor = 'rgb(50, 50, 50)';
+    // document.getElementById("toggle_button").style.backgroundColor = 'rgb(215, 215, 215)';
     document.querySelectorAll("*").forEach(doc => doc.style.color = 'rgb(92, 230, 96)');
     document.querySelectorAll("hr").forEach(doc => doc.style.backgroundColor = 'rgb(92, 230, 96)');
     current_mode = "dark";
