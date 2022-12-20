8b2c60d78c4c5c30242666c8b5731468ca36ec44
blloop <42186044+blloop@users.noreply.github.com>
2022-12-20 00:39:37 -0800

    add light mode feature

---
+++ script.js
@@ -0,0 +1,25 @@
+let current_mode = "dark";
+
+function toggle_color() {
+
+    (current_mode === "dark" ? set_light() : set_dark());
+
+};
+
+function set_light() {
+    document.getElementById("toggle_button").innerText = "LIGHT MODE";
+    document.body.style.backgroundColor = 'rgb(215, 215, 215)';
+    document.getElementById("toggle_button").style.backgroundColor = 'rgb(215, 215, 215)';
+    document.querySelectorAll("*").forEach(doc => doc.style.color = 'rgb(50, 50, 50)');
+    document.querySelectorAll("hr").forEach(doc => doc.style.backgroundColor = 'rgb(50, 50, 50)');
+    current_mode = "light";
+}
+
+function set_dark() {
+    document.getElementById("toggle_button").innerText = "DARK MODE";
+    document.body.style.backgroundColor = 'rgb(50, 50, 50)';
+    document.getElementById("toggle_button").style.backgroundColor = 'rgb(50, 50, 50)';
+    document.querySelectorAll("*").forEach(doc => doc.style.color = 'rgb(92, 230, 96)');
+    document.querySelectorAll("hr").forEach(doc => doc.style.backgroundColor = 'rgb(92, 230, 96)');
+    current_mode = "dark";
+}
