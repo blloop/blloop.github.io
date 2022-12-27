46f4936dc508c2cdb0e3067f8b4e0e8a5d9a8660
blloop <42186044+blloop@users.noreply.github.com>
2022-12-26 18:18:44 -0800

    add content, modularize styles

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 let current_mode = "dark";
 
+// Toggles mode between light and dark
 function toggle_color() {
-
     (current_mode === "dark" ? set_light() : set_dark());
-
 };
 
+// Modifies page styles to mimic light mode
 function set_light() {
-    document.getElementById("lightbulb").src = "img/light_off.png";
     document.body.style.backgroundColor = 'rgb(215, 215, 215)';
-    // document.getElementById("toggle_button").style.backgroundColor = 'rgb(50, 50, 50)';
-    document.querySelectorAll("*").forEach(doc => doc.style.color = 'rgb(50, 50, 50)');
-    document.querySelectorAll("hr").forEach(doc => doc.style.backgroundColor = 'rgb(50, 50, 50)');
+    document.getElementById("lightbulb").src = "img/light_off.png";
+    document.querySelectorAll(".toggle").forEach(doc => doc.style.color = 'rgb(50, 50, 50)');
+    document.querySelectorAll(".toggle-bg").forEach(doc => doc.style.backgroundColor = 'rgb(50, 50, 50)');
+    document.querySelectorAll(".toggle-contrast").forEach(doc => doc.style.backgroundColor = 'rgb(150, 150, 150)');
     current_mode = "light";
 }
 
+// Modifies page styles to mimic dark mode
 function set_dark() {
-    document.getElementById("lightbulb").src = "img/light_on.png";
     document.body.style.backgroundColor = 'rgb(50, 50, 50)';
-    // document.getElementById("toggle_button").style.backgroundColor = 'rgb(215, 215, 215)';
-    document.querySelectorAll("*").forEach(doc => doc.style.color = 'rgb(92, 230, 96)');
-    document.querySelectorAll("hr").forEach(doc => doc.style.backgroundColor = 'rgb(92, 230, 96)');
+    document.getElementById("lightbulb").src = "img/light_on.png";
+    document.querySelectorAll(".toggle").forEach(doc => doc.style.color = 'rgb(92, 230, 96)');
+    document.querySelectorAll(".toggle-bg").forEach(doc => doc.style.backgroundColor = 'rgb(92, 230, 96)');
+    document.querySelectorAll(".toggle-contrast").forEach(doc => doc.style.backgroundColor = 'rgb(215, 215, 215)');
     current_mode = "dark";
 }
