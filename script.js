dfab9e88a7279ada9ac0509510bad91119944865
blloop <42186044+blloop@users.noreply.github.com>
2022-12-28 17:47:15 -0800

    minor style adjustments

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let current_mode = "dark";
 
+let white_rgb = 'rgb(215, 215, 215)';
+let grey_rgb = 'rgb(150, 150, 150)';
+let dark_grey_rgb = 'rgb(50, 50, 50)';
+let green_rgb = 'rgb(92, 230, 96)';
+
 // Toggles mode between light and dark
 function toggle_color() {
     (current_mode === "dark" ? set_light() : set_dark());
@@ -7,20 +12,24 @@ function toggle_color() {
 
 // Modifies page styles to mimic light mode
 function set_light() {
-    document.body.style.backgroundColor = 'rgb(215, 215, 215)';
+    document.body.style.backgroundColor = white_rgb;
     document.getElementById("lightbulb").src = "img/light_off.png";
-    document.querySelectorAll(".toggle").forEach(doc => doc.style.color = 'rgb(50, 50, 50)');
-    document.querySelectorAll(".toggle-bg").forEach(doc => doc.style.backgroundColor = 'rgb(50, 50, 50)');
-    document.querySelectorAll(".toggle-contrast").forEach(doc => doc.style.backgroundColor = 'rgb(150, 150, 150)');
+    document.querySelectorAll(".toggle").forEach(doc => doc.style.color = dark_grey_rgb);
+    document.querySelectorAll(".toggle-bg").forEach(doc => doc.style.backgroundColor = dark_grey_rgb);
+    document.querySelectorAll(".toggle-contrast").forEach(doc => doc.style.backgroundColor = grey_rgb);
     current_mode = "light";
 }
 
 // Modifies page styles to mimic dark mode
 function set_dark() {
-    document.body.style.backgroundColor = 'rgb(50, 50, 50)';
+    document.body.style.backgroundColor = dark_grey_rgb;
     document.getElementById("lightbulb").src = "img/light_on.png";
-    document.querySelectorAll(".toggle").forEach(doc => doc.style.color = 'rgb(92, 230, 96)');
-    document.querySelectorAll(".toggle-bg").forEach(doc => doc.style.backgroundColor = 'rgb(92, 230, 96)');
-    document.querySelectorAll(".toggle-contrast").forEach(doc => doc.style.backgroundColor = 'rgb(215, 215, 215)');
+    document.querySelectorAll(".toggle").forEach(doc => doc.style.color = green_rgb);
+    document.querySelectorAll(".toggle-bg").forEach(doc => doc.style.backgroundColor = green_rgb);
+    document.querySelectorAll(".toggle-contrast").forEach(doc => doc.style.backgroundColor = white_rgb);
     current_mode = "dark";
 }
+
+window.addEventListener("load", () => {
+    set_dark();
+});
