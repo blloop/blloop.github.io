f4ee927feeae2072c7f966534f7c7e21b338eea2
blloop <42186044+blloop@users.noreply.github.com>
2023-02-01 00:20:18 -0800

    add pipe interactions

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let rotations = [2, 1, 2, 1, 2, 0, 3, 0, 3, 0];
 
-for (let i = 0; i < rotations.length; i++) {
-    let out = `rotate(${90 * rotations[i]}deg)`;
-    document.getElementById(i.toString()).style.transform = out;
+let updateAll = () => {
+    for (let i = 0; i < rotations.length; i++) {
+        let out = `rotate(${90 * rotations[i]}deg)`;
+        document.getElementById(i.toString()).style.transform = out;
+    }
 }
 
 spin = (val) => {
@@ -10,3 +12,12 @@ spin = (val) => {
     rotations[val] = (rotations[val] + 1) % 16;
     pipe.style.transform = `rotate(${90 * rotations[val]}deg)`;
 }
+
+randomize = () => {
+    for (let i = 0; i < rotations.length; i++) {
+        rotations[i] = Math.floor(Math.random() * 4);
+    }
+    updateAll();
+}
+
+updateAll();
